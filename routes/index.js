0019598c218601e4005d19ceb07584f36a90d97a
zhang <www.1020929225@qq.com>
2018-11-26 16:31:12 +0800

    这是搭了一半的页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/register.html', function(req, res, next) {
+  res.render('register');
+});
+router.get('/login.html', function(req, res, next) {
+  res.render('login');
+});
+router.get('/userManage.html', function(req, res, next) {
+  res.render('userManage');
+});
+router.get('/phoneManage.html', function(req, res, next) {
+  res.render('phoneManage');
+});
+router.get('/brandManage.html', function(req, res, next) {
+  res.render('brandManage');
+});
 module.exports = router;
